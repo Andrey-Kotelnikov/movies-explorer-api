2932e047a079b90185876f5e95ebe8c129c5937e
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-09-26 17:14:20 +0300

    feat: бэкенд на локалке

---
+++ controllers/movies.js
@@ -0,0 +1,86 @@
+const Movie = require("../models/movie");
+// const user = require('../models/user')
+
+const ValidationError = require("../errors/validation-error");
+const NotFoundError = require("../errors/not-found-error");
+const AccessError = require("../errors/access-error");
+
+// Получение всех сохраненных фильмов
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+// Создание фильма
+module.exports.createMovie = (req, res, next) => {
+  // const { name, link } = req.body;
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+    owner: req.user,
+  })
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "ValidationError") {
+        next(
+          new ValidationError(
+            `${Object.values(err.errors)
+              .map((error) => error.message)
+              .join(", ")}`,
+          ),
+        );
+        return;
+      }
+      next(err);
+    });
+};
+
+// Удаление сохраненного фильма по id
+module.exports.deleteMovie = (req, res, next) => {
+  const { _id } = req.user;
+  Movie.findOne({ _id: req.params.movieId })
+    .orFail(new NotFoundError("Фильм не найден"))
+    .then((movie) => {
+      console.log(`создатель фильма: ${movie.owner._id}`);
+      console.log(`твой id: ${req.user._id}`);
+      if (!movie.owner.equals(_id)) {
+        throw new AccessError("Нельзя удалять фильмы других пользователей");
+      }
+      Movie.deleteOne(movie)
+        .then(() => res.send({ data: movie }))
+        .catch(next);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "CastError") {
+        next(new ValidationError("Некорректный id"));
+        return;
+      }
+      next(err);
+    });
+};
---
+++ middlewares/joi-validation.js
@@ -0,0 +1,24 @@
+const { celebrate, Joi } = require("celebrate");
+
+const validationSignin = celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+    })
+    .unknown(true),
+});
+
+const validationSignup = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
+module.exports = {
+  validationSignin,
+  validationSignup,
+};
